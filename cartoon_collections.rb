3020a9bac0416abe516ed95d673d8f986d3a54cf
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 08:50:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ cheese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
 find_the_cheese(snacks)
 #=> "gouda"
- 
+
 soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 find_the_cheese(soup)
 
