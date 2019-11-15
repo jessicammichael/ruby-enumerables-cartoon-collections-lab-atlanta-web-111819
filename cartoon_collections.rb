e7d6caf7e7def6a12aa97c5988b9388556c16db9
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 08:50:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,32 @@
 '''ruby
 
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves
   dwarves = ["Dopey", "Mopey", "Charlie", "Bant", "Boo", "Frankie", "Sir"]
   %w(dwarves).each_with_index { |item, index|
     hash[item] = index
 end
 
-def summon_captain_planet# code an argument here
-
+def summon_captain_planet
+planeteer_calls = ["mercury", "jupiter", "mars", "moon", "saturn"]
   %w(summon_captain_planet).each_with_index { |item, index|
     hash[item] = index
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls
+  long_planeteer_calls = []
   %w(long_planeteer_calls).each_with_index { |item, index|
     hash[item] = index
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese
 cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+#=> "gouda"
+ 
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
+
 %w(cheese_types).each_with_index { |item, index|
   hash[item] = index
 end
